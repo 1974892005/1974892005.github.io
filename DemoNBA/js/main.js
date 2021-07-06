f57b3eb06aa7bff6ecc338718582dc9a8cfd186d
Dibin-Test <1974892005@qq.com>
2021-07-06 18:30:12 +0800

    updata

--- DemoNBA/js/main.js
+++ DemoNBA/js/main.js
@@ -30,17 +30,17 @@ var app = new Vue({
                     "dec": " 20前锋 /  雄鹿",
                     "score": 32
                 }, {
-                    "url": "https://china.nba.com/media/img/players/silos/440x700/203114.png",
+                    "url": "https://china.nba.com/media/img/players/silos/440x700/201950.png",
                     "name": "克里斯 米德尔顿",
                     "dec": " 20前锋 /  雄鹿",
                     "score": 32
                 }, {
-                    "url": "https://china.nba.com/media/img/players/silos/440x700/203114.png",
+                    "url": "https://china.nba.com/media/img/players/silos/440x700/201572.png",
                     "name": "克里斯 米德尔顿",
                     "dec": " 20前锋 /  雄鹿",
                     "score": 32
                 }, {
-                    "url": "https://china.nba.com/media/img/players/silos/440x700/203114.png",
+                    "url": "https://china.nba.com/media/img/players/silos/440x700/1628381.png",
                     "name": "克里斯 米德尔顿",
                     "dec": " 20前锋 /  雄鹿",
                     "score": 32
@@ -55,8 +55,39 @@ var app = new Vue({
                 "//mat1.gtimg.com/chinanba/web/statics/logo_partner_01_79da27.jpg", "//mat1.gtimg.com/chinanba/web/statics/logo_partner_02_084a68.jpg", "//img1.gtimg.com/mobilenbachina/pics/hv1/204/14/2291/148976049.png", "//mat1.gtimg.com/chinanba/web/statics/changhong_102d6e.png", "//mat1.gtimg.com/chinanba/web/statics/qingyang-nanshi_23be81.png", "//mat1.gtimg.com/chinanba/web/statics/migu_8d29fc.jpg", "//img1.gtimg.com/chinanba/pics/hv1/96/18/2324/151122786.jpg"
             ],
             currentDate: new Date(),
-            videoUrl: 'http://inews.gtimg.com/newsapp_ls/0/13720271693_1164606/0',
-            VideoData: "2021-07-05"
+            VideoData: "2021-07-05",
+            seritem: [
+                {
+                    "serimg": "http://inews.gtimg.com/newsapp_ls/0/13734779270_1164606/0",
+                    "sertitle": "47年与28年 雄鹿和太阳终于重返总决赛"
+                },
+                {
+                    "serimg": "http://inews.gtimg.com/newsapp_ls/0/13730049664_1164606/0",
+                    "sertitle": "雄鹿和太阳终于重返总决赛"
+                }
+            ],
+            videoUrl: [
+                {
+                    "url": "http://inews.gtimg.com/newsapp_ls/0/13720271693_1164606/0"
+                }, {
+                    "url": "http://inews.gtimg.com/newsapp_ls/0/13727009992_1164606/0"
+                }, {
+                    "url": "http://inews.gtimg.com/newsapp_ls/0/13727461638_1164606/0"
+                }
+            ],
+            special: [
+                {
+                    "ImgUrl": "https://img1.gtimg.com/chinanba/pics/hv1/195/15/2284/148521120.jpg",
+                    "pics": "http://img1.gtimg.com/chinanba/pics/hv1/32/15/2326/151252007.jpg"
+                },
+                {
+                    "ImgUrl": "https://img1.gtimg.com/chinanba/pics/hv1/118/16/2284/148521298.jpg",
+                    "pics": "http://img1.gtimg.com/chinanba/pics/hv1/38/15/2326/151252013.jpg"
+                }, {
+                    "ImgUrl": "https://mat1.gtimg.com/chinanba/web/statics/zt_nba5V5_2cb751.jpg",
+                    "pics": "http://img1.gtimg.com/chinanba/pics/hv1/41/15/2326/151252016.jpg"
+                }
+            ]
         }
     }
 })
